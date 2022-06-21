f647e9e61aa6f52d192ff4937e9fa8762a805028
Roman Khabirov <khabirov_rr@bk.ru>
2022-06-21 21:33:57 +0300

    Бинарный поиск

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+
+
+def binary_search(n: int, sorted_array: list):
+	low = 0
+	high = len(sorted_array) - 1
+	while low <= high:
+		mid = (low + high) // 2
+		guess = sorted_array[mid]
+
+		if guess > n:
+			high = mid - 1
+		elif guess < n:
+			low = mid + 1
+		else:
+			return mid
+
+	return None
+
+
+if __name__ == '__main__':
+	print(binary_search(2, [1,2,3]))
